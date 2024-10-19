75a1bbadb484cbe5028956e71083a6059bdde86f
John Khalife <jpa.khalife@gmail.com>
2024-10-19 16:45:32 -0400

    - added not found page

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -4,7 +4,9 @@ import "./NotFound.style.scss";
 function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
-      <h1>404</h1>
+      <h1 id='title'>404</h1>
+      <p id='text'>You've made it to the Chamber of Secrets! <br></br> In all seriousness though, that url doesn't exist.</p>
+      <img id='chamber' src='/secret.png'></img>
     </Layout>
   );
 }
