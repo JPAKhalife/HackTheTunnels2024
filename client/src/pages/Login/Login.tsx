01dfc594ce5559871df4413d629723d09eb5873e
John Khalife <jpa.khalife@gmail.com>
2024-10-19 12:34:58 -0400

    - Removed carleton background image - Added image of hogwarts - Changed incorrect account and password information message - Added a Harry Potter Font to the error messages - Added a Harry potter Logo - Moved panel to the left - changed error font to scary font - Changed description text to harry potter font

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -21,29 +21,52 @@ function Login() {
       <div className="Login"></div>
       <div className="Login__panel">
         <div className="Login__panel__content">
-          <img src="/carleton_logo_black.png"></img>
+          <img src="/hogwartslogo.png"></img>
           <div className="Login__panel__content__message">
-            <div>Welcome to the Carleton SSO Federated Portal.</div>
-            <div>
+            <div id="description">Welcome to the Hogwarts SSO Federated Portal.</div> 
+            <br></br>
+            <div id="description">That's right, we use technology too!</div>
+            <div id="description">
               Enter your{" "}
-              <a href="https://myone.carleton.ca" target="blank">
-                MyCarletonOne
+              <a href="https://www.hogwartsishere.com/courses/by-level/" target="blank">
+                Hogwarts
               </a>{" "}
               username and password.
             </div>
           </div>
-          {message && <p>{message}</p>}
+          {message && <p id="error">{message}</p>}
           <div className="Login__panel__content__input">
-            <input type="text" placeholder="MyCarletonOne username"></input>
+            <input type="text" placeholder="Hogwarts username"></input>
             <input type="password" placeholder="Password"></input>
           </div>
           <div className="Login__panel__content__checkbox">
             <input type="checkbox"></input>
             <label>Keep me signed in</label>
           </div>
+          <div className="buttons">
+          <button className="blob-btn" onClick={() => attemptLogin()}>
+            Sign In
+            <span className="blob-btn__inner">
+              <span className="blob-btn__blobs">
+                <span className="blob-btn__blob"></span>
+                <span className="blob-btn__blob"></span>
+                <span className="blob-btn__blob"></span>
+                <span className="blob-btn__blob"></span>
+              </span>
+            </span>
+          </button>
+          </div>
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+              <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+            </filter>
+          </defs>
+        </svg>
           <button
-            className="Login__panel__button"
-            onClick={() => attemptLogin()}
+            
           >
             Sign In
           </button>
